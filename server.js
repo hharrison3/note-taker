0b5be1653bdd6f5fd314115cc1894737c51cae4d
Harbour Harrison <harb3gaucho@gmail.com>
2020-09-30 11:24:56 -0700

    created routes for HTML

---
+++ server.js
@@ -0,0 +1,45 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Example Note (DATA)
+// =============================================================
+var notes = [
+    {
+    title:"Test Title",
+    text:"Test text"
+    }
+];
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "Develop/public/notes.html"));
+}); 
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "Develop/public/index.html"));
+}); 
+
+
+
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
